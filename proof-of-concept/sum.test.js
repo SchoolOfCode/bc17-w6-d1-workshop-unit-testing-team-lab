51422142e2db78a747d692fb1374ac7e1e47d761
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-08-05 12:25:09 +0100

    task 2

--- proof-of-concept/sum.test.js
+++ proof-of-concept/sum.test.js
@@ -1,6 +1,8 @@
 import { expect, test } from 'vitest'
 import { sum } from './sum.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+test('adds 5 + 7 to equal 12', () => {
+    const total = sum(5,7);
+    const expected = 12;
+  expect(total).toBe(expected)
 })
